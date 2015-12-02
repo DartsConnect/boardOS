0cc963d8fc50bf2f5c5364b33a4fa5a114b2f9ca
Jordan Lewis <jordan_j_lewis@icloud.com>
2015-12-02 11:51:27 +1100

    Refactor Controller.java in main to MainSceneController.java Link MainSceneController.java to the main scene (fxml)

---
+++ Kiosk/src/main/MainSceneController.java
@@ -0,0 +1,22 @@
+package main;
+
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainSceneController implements Initializable {
+
+    private void transitionToScene(Scene destination) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("View loaded!");
+    }
+
+    public MainSceneController() {
+
+    }
+}
