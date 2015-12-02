6ab58351059a9262da20ae2cefe4d95701d26ebf
Jordan Lewis <jordan_j_lewis@icloud.com>
2015-12-02 11:50:37 +1100

    Tie Register User Scene to Controller file

---
+++ Kiosk/src/register/RegisterUserController.java
@@ -0,0 +1,30 @@
+package register;
+
+/**
+ * Created by JordanLewis on 2/12/2015.
+ */
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterUserController implements Initializable {
+
+    private void cancel() {
+        // Return to the main scene
+    }
+
+    private boolean registerUser(String username, String email, String password) {
+
+        return true;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("View is now loaded!");
+    }
+
+    public RegisterUserController() {
+
+    }
+}
