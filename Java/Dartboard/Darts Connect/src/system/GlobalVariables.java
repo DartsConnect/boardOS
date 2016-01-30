78c6be27e0296b323c2b29849f87e1561f865979
Jordan Lewis <jordan_j_lewis@icloud.com>
2016-01-30 21:53:17 +1100

    Add index of code for primitive int array

--- Java/Dartboard/Darts Connect/src/system/GlobalVariables.java
+++ Java/Dartboard/Darts Connect/src/system/GlobalVariables.java
@@ -6,6 +6,7 @@ import system.hardware_input.KeyboardListener;
 /**
  * Created by JordanLewis on 22/10/2015.
  */
+
 public class GlobalVariables {
     private static GlobalVariables ourInstance = new GlobalVariables();
 
@@ -16,6 +17,17 @@ public class GlobalVariables {
     public static KeyboardListener keyboardListener;
 
     public static Scene currentScene;
+
+    public int indexOf(int item, int[] arr) {
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
 
     private GlobalVariables() {
     }
